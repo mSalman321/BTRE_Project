7910872206e74c474bebc4ad636d56fb2499169a
abc <abc@gmail.com>
2020-02-17 13:20:03 +0530

    Admin Panel listing

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import Listing
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','photo_main','address','list_date'  ,'realtor', 'is_published')
+    # to make links clickable
+
+    list_display_links = ('id', 'title')
+
+    list_filter = ('realtor','price','is_published', )
+
+    # list_editable = ('is_published',)
+
+    search_fields = ('title', 'description', 'address' , 'city', 'zipcode')
+
+    list_per_page = 25;
+
+
+
+
+
+admin.site.register(Listing,ListingAdmin)
