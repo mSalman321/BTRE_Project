398b865b349e60e944acccd012e3e0ac8a88e59f
abc <abc@gmail.com>
2020-02-18 10:29:36 +0530

    Admin Panel listing

--- listings/admin.py
+++ listings/admin.py
@@ -6,11 +6,11 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ('id','title','photo_main','address','list_date'  ,'realtor', 'is_published')
     # to make links clickable
 
-    list_display_links = ('id', 'title')
+    list_display_links = ('id', 'title',)
 
     list_filter = ('realtor','price','is_published', )
 
-    # list_editable = ('is_published',)
+    list_editable = ('is_published',)
 
     search_fields = ('title', 'description', 'address' , 'city', 'zipcode')
 
